b3454b19329b345fdadb75cd90bea22a043f5444
oviozz <itspraz81@gmail.com>
2024-02-16 16:48:49 -0800

    Update: Add

--- src/hooks/useBuildingChoose.jsx
+++ src/hooks/useBuildingChoose.jsx
@@ -20,12 +20,8 @@ const BuildingChooseProvider = ({ children }) => {
     const { surveyID } = useParams();
 
     const saveBuildingtoDB = async () => {
-<<<<<<< HEAD
 
         const response = await fetch(`https://propertyestate.vercel.app/createBuilding?survey_id=${surveyID}`, {
-=======
-        const response = await fetch(`https://propertyestate.vercel.app/createBuilding?survey_id=${survey}`, {
->>>>>>> bdf70fac3d88e3e783d14c32315889430b7c6d8b
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -38,9 +34,6 @@ const BuildingChooseProvider = ({ children }) => {
 
     }
 
-    const setChooseData = (cardChosenData) => {
-        setcardChosenData(JSON.stringify(cardChosenData));
-    }
 
     const fetchBuildingData = async (id, cardChosenData) => {
 
