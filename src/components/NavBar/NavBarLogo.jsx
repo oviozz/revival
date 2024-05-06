97316352a3356713da919f547a229523888f51d1
oviozz <itspraz81@gmail.com>
2024-05-05 18:32:20 -0700

    apple

--- src/components/NavBar/NavBarLogo.jsx
+++ src/components/NavBar/NavBarLogo.jsx
@@ -1,10 +1,11 @@
 
+import SendCRE_LogosFinal from "../../assets/SendCRE_LogosFinal.png"
 
 const NavBarLogo = () => {
 
     return (
         <div>
-            <img  width={220} src={"SendCRE_LogosFinal.png"} />
+            <img  width={220} src={SendCRE_LogosFinal} />
         </div>
     )
 
